6c8849f6ea7f5f5f1fb851eeaf617efcf83a5c59
sasata299 <karaage299@gmail.com>
2018-12-24 08:47:06 +0900

    コメントを取得して絵文字のランキングを表示する

--- slack-emoji-checker.rb
+++ slack-emoji-checker.rb
@@ -1,11 +1,31 @@
 require 'slack-ruby-client'
+require 'pry'
 
 Slack.configure do |config|
   config.token = ENV['SLACK_API_TOKEN']
 end
 
+reaction_counts = Hash.new { |h,k| h[k] = 0 }
+
 client = Slack::Web::Client.new
-client.channels_list.channels.each do |channel|
-  p channel.id
+
+client.conversations_list(types: 'public_channel,private_channel', limit: 500).channels.each do |channel|
+  next unless channel.name == ENV['TARGET_CHANNEL']
   p channel.name
+
+  history = client.conversations_history(channel: channel.id, count: 3)
+  history.messages.each do |message|
+    p message.text
+
+    if message.reactions
+      message.reactions.each do |reaction|
+        reaction_counts[reaction.name] += 1
+      end
+    end
+  end
+end
+
+reaction_counts.sort { |(k1,v1),(k2,v2)| v2 <=> v1 }.each_with_index do |(k,v), i|
+  p "#{i+1}: #{k}"
+  exit if i >= 20
 end
